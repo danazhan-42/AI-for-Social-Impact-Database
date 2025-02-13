9fe22273512c9c81694f2cd04e1d37ff4893475a
Dana Zhan <62995082+danazhan-42@users.noreply.github.com>
2025-02-12 16:35:34 -0800

    chore: setup middleware for Express

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { auth } from "express-openid-connect";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -13,12 +15,30 @@ const config = {
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
 };
+
 app.use(auth(config));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 // app.get("/random", (req, res) => {
 //   res.json({ random: Math.random() });
 // });
 
-app.listen(process.env.PORT, () => {
-  console.log(`server is listening to ${process.env.PORT}`);
-});
+const server = async () => {
+  try {
+    app.listen(process.env.PORT, () => {
+      console.log(`server is listening to ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Server error", error.message);
+    process.exit(1);
+  }
+};
+
+server();
